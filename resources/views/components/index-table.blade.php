d769987dd3e64dbd2d634218617010910b6ae1f3
Cynthia Rubio <cynrusan@gmail.com>
2026-03-26 20:12:52 +0100

    style: add dark mode styles to shared index table component

--- resources/views/components/index-table.blade.php
+++ resources/views/components/index-table.blade.php
@@ -1,7 +1,7 @@
 @if($hasItems)
     <div class="mt-4 overflow-x-auto">
         <table class="min-w-full text-base">
-            <thead class="bg-slate-50 text-base uppercase tracking-wide text-slate-500">
+            <thead class="bg-slate-50 text-base uppercase tracking-wide text-slate-500 dark:bg-slate-800 dark:text-slate-300">
                 <tr>
                     @foreach($headers as $header)
                         <th class="px-3 py-2 text-left">{{ $header }}</th>
@@ -9,7 +9,7 @@
                 </tr>
             </thead>
 
-            <tbody class="divide-y divide-slate-100 bg-white">
+            <tbody class="divide-y divide-slate-100 bg-white dark:divide-slate-700 dark:bg-slate-900">
                 {{ $slot }}
             </tbody>
         </table>
@@ -21,5 +21,5 @@
         </div>
     @endif
 @else
-    <div class="mt-4 text-base text-slate-500">{{ $emptyText }}</div>
+    <div class="mt-4 text-base text-slate-500 dark:text-slate-400">{{ $emptyText }}</div>
 @endif
